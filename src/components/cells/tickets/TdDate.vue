bf2b0363c64c6605dc9f63382b220ba4e3fdde14
Phạm Xuân Duy <xduy.pham@gmail.com>
2019-09-08 21:21:58 +0700

    task date

--- src/components/cells/tickets/TdDate.vue
+++ src/components/cells/tickets/TdDate.vue
@@ -1,11 +1,35 @@
 <template>
-  <td>{{dataRow.received_date}}</td>
+  <td>
+    <v-menu v-model="menu" transition="scale-transition" offset-y style="width: 100px">
+      <template v-slot:activator="{ on }">
+        <span class="cursor" v-on="on">{{ dataRow.received_date | formatDaysUtc }}</span>
+      </template>
+      <v-date-picker
+        @change="$emit('update', new Date($event).toISOString())"
+        no-title
+        :value="dataRow.received_date"
+        width="290"
+        class="elevation-3"
+      ></v-date-picker>
+    </v-menu>
+  </td>
 </template>
+
 <script>
 export default {
   props: {
     dataRow: Object,
     column: Object
+  },
+  data() {
+    return {
+      menu: false
+    };
   }
 };
 </script>
+<style scoped>
+.cursor {
+  cursor: pointer;
+}
+</style>

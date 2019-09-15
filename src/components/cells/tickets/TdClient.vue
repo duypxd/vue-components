5e583db34f5fe4de90de80f3214e33f79fb7e8cd
Phạm Xuân Duy <xduy.pham@gmail.com>
2019-09-14 18:54:25 -0700

    final table tickets

--- src/components/cells/tickets/TdClient.vue
+++ src/components/cells/tickets/TdClient.vue
@@ -1,5 +1,14 @@
 <template>
-  <td>{{dataRow.client_name}}</td>
+  <td>
+    <v-textarea
+      hide-details
+      name="input-7-1"
+      :value="dataRow.client_name"
+      class="ma-0 pa-0 custom-text-field input-size"
+      @change="$emit('update', $event)"
+      rows="1"
+    ></v-textarea>
+  </td>
 </template>
 <script>
 export default {
@@ -9,3 +18,12 @@ export default {
   }
 };
 </script>
+<style scoped>
+.custom-text-field /deep/ .v-input__slot::before {
+  border-style: unset;
+}
+.input-size {
+  width: 130px;
+  font-size: 13px;
+}
+</style>
--- src/components/cells/tickets/TdPriority.vue
+++ src/components/cells/tickets/TdPriority.vue
@@ -1,11 +1,77 @@
 <template>
-  <td>{{dataRow.priority}}</td>
+  <td>
+    <v-chip small style="width: 100px" :color="showClassColor(nameCustom.name)" text-color="white">
+      <v-menu
+        style="width: 130px"
+        v-model="menu"
+        transition="scale-transition"
+        offset-y
+        :close-on-content-click="false"
+      >
+        <template v-slot:activator="{ on }">
+          <span v-on="on" class="name">{{ nameCustom.name }}</span>
+        </template>
+        <v-list>
+          <v-row no-gutters v-for="(item, index) in priority" :key="index" class="ma-2">
+            <v-btn
+              class="px-2 white--text text-capitalize"
+              small
+              :color="showClassColor(item.name)"
+              @click="$emit('update', item.id); menu = false"
+            >{{ item.id }}: {{ item.name }}</v-btn>
+          </v-row>
+        </v-list>
+      </v-menu>
+    </v-chip>
+  </td>
 </template>
 <script>
+import { priority } from "../../../config";
 export default {
+  data() {
+    return {
+      priority,
+      menu: false
+    };
+  },
   props: {
     dataRow: Object,
     column: Object
+  },
+  computed: {
+    nameCustom() {
+      return this.priority.find(x => x.id === this.dataRow.priority);
+    }
+  },
+  methods: {
+    showClassColor(name) {
+      if (name === "Very low") {
+        return "#512da8";
+      } else if (name === "Very high") {
+        return "#ff6f00";
+      } else if (name === "Low") {
+        return "#54979F";
+      } else if (name === "Normal") {
+        return "#f50057";
+      } else if (name === "High") {
+        return "#00c853";
+      }
+    }
   }
 };
 </script>
+<style scoped>
+.showBackground {
+  border-radius: 10px;
+}
+.changeSelect {
+  text-align: center;
+  color: white;
+  border: 3px solid transparent;
+  background-clip: padding-box;
+}
+.name {
+  cursor: pointer;
+  width: 100px;
+}
+</style>
